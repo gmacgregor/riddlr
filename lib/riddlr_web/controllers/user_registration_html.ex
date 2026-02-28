e4e5c3d004c313c050a41cfe43bcc3d132fa1a04
gmacgregor <gmacgregor@gmail.com>
2026-02-27 23:17:55 -0500

    feat: add user authentication with phx.gen.auth

---
+++ lib/riddlr_web/controllers/user_registration_html.ex
@@ -0,0 +1,5 @@
+defmodule RiddlrWeb.UserRegistrationHTML do
+  use RiddlrWeb, :html
+
+  embed_templates "user_registration_html/*"
+end
---
+++ lib/riddlr_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule RiddlrWeb.UserSettingsHTML do
+  use RiddlrWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
